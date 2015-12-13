da842e2f0156bc68851c326cb306a816931f4e32
Mario E Salvatierra V <MarioSalvatierra@marios-mbp-2>
2015-12-12 20:54:58 -0500

    velocidades de auto

---
+++ MyPlayWeek4Tarea.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - Auto y velocidades con Class and Enum
+// by Mario E. SV
+import UIKit
+
+enum Velocidades : Int {
+    case Apagado = 0, Velocidad_Baja = 20, Velocidad_Media = 50, Velocidad_Alta = 120
+    
+    init(velocidadInicial: Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad: Velocidades
+    
+    init() {
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad()-> (actual: Int, velocidadEnCadena: String) {
+        let temporal = self.velocidad
+        
+        switch self.velocidad {
+        case .Apagado:
+            self.velocidad = .Velocidad_Baja
+        case .Velocidad_Baja:
+            self.velocidad = .Velocidad_Media
+        case .Velocidad_Media:
+            self.velocidad = .Velocidad_Alta
+        case .Velocidad_Alta:
+            self.velocidad = .Velocidad_Media
+        }
+        return (temporal.rawValue, "\(temporal)")
+    }
+}
+
+let auto = Auto()
+for i in 0..<20 {
+    let (velocimetro, velocidad) = auto.cambioDeVelocidad()
+    print("\(velocimetro), \(velocidad) " )
+    }
+
+
